fa51eb4348c57c584d283d531f64edd468252fc4
Gavin King <gavin@hibernate.org>
2024-03-24 00:47:35 +0100

    Initial commit

---
+++ src/main/java/org/example/Library.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import jakarta.data.Sort;
+import jakarta.data.repository.Delete;
+import jakarta.data.repository.Find;
+import jakarta.data.repository.Insert;
+import jakarta.data.repository.Query;
+import jakarta.data.repository.Repository;
+import jakarta.transaction.Transactional;
+import org.hibernate.annotations.processing.Pattern;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface Library {
+
+    @Find
+    Optional<Book> byIsbn(String isbn);
+
+    @Find
+    List<Book> byTitle(@Pattern String title);
+
+    @Insert
+    void add(Book book);
+
+    @Delete
+    void delete(String isbn);
+
+    @Find
+    List<Book> booksSorted(Sort<Book> bookSort);
+
+    record Summary(String isbn, String title, String authors) {}
+    @Query("select b.isbn, b.title, listagg(a.name, ' & ') "
+            + "from Book b join b.authors a "
+            + "group by b "
+            + "order by b.isbn")
+    List<Summary> summarize();
+
+}
---
+++ src/main/java/org/example/LibraryResource.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import java.util.List;
+
+@Path("/")
+public class LibraryResource {
+
+    @Inject Library library;
+
+    @GET
+    @Path("/book/{isbn}")
+    public Book byIsbn(@PathParam String isbn) {
+        return library.byIsbn(isbn)
+                .orElseThrow(() ->new NotFoundException(isbn));
+    }
+
+    @GET
+    @Path("/title/{title}")
+    public List<Book> byTitle(@PathParam String title) {
+        String pattern = '%' + title.replace('*', '%') + '%';
+        return library.byTitle(pattern);
+    }
+
+    @GET
+    @Path("/books")
+    public  List<Book> allBooks() {
+        return library.booksSorted(_Book.title.ascIgnoreCase());
+    }
+
+    @POST
+    @Path("/new")
+    public String create(Book book) {
+        library.add(book);
+        return "Added " + book.isbn;
+    }
+
+    @POST
+    @Path("/delete/{isbn}")
+    @Transactional
+    public String delete(@PathParam String isbn) {
+        library.delete(isbn);
+        return "Deleted " + isbn;
+    }
+
+    @GET
+    @Path("/summary")
+    public  List<Library.Summary> summary() {
+        return library.summarize();
+    }
+
+}
